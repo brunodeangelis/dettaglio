b49ed0354de4d05cc03a47ca9c791be20d2d1648
Bruno De Angelis <41843151+brunodeangelis@users.noreply.github.com>
2019-07-15 00:28:20 -0300

    release v1

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,33 +1,83 @@
 <template>
-  <Layout>
-    
-    <!-- Learn how to use images here: https://gridsome.org/docs/images -->
-    <g-image alt="Example image" src="~/favicon.png" width="135" />
-    
-    <h1>Hello, world!</h1>
-   
-    <p>
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur excepturi labore tempore expedita, et iste tenetur suscipit explicabo! Dolores, aperiam non officia eos quod asperiores
-    </p>
-
-    <p class="home-links">
-      <a href="https://gridsome.org/docs" target="_blank" rel="noopener">Gridsome Docs</a>
-      <a href="https://github.com/gridsome/gridsome" target="_blank" rel="noopener">GitHub</a>
-    </p>
-
+  <Layout class="container mx-auto px-5 md:px-0">
+    <header>
+      <div class="flex justify-center">
+        <g-image src="@/assets/logo.png" alt="logo Dettaglio" />
+      </div>
+      <div class="text-center mt-8">
+        <h1 class="text-4xl text-primary">We create digital products</h1>
+      </div>
+    </header>
+    <main class="mt-16">
+      <div class="flex flex-col md:flex-row md:justify-center">
+        <article
+          v-for="(user, i) in users"
+          :key="i"
+          class="mb-12 mx-auto md:mb-0 md:px-6 lg:px-8 xl:px-16"
+        >
+          <figure>
+            <g-image
+              :src="`/img/${user.pic}`"
+              :alt="`profile photo of ${user.name}`"
+              class="rounded-lg mb-4"
+            />
+            <figcaption>
+              <h2 class="text-4xl text-primary font-bold">{{ user.name }}</h2>
+              <h3 class="tracking-wide uppercase text-xl text-gray-600">{{ user.role }}</h3>
+            </figcaption>
+          </figure>
+          <div class="flex mt-4">
+            <div v-for="(method, index) in user.contactMethods" :key="index" class="mr-2">
+              <a v-if="method.name == 'mail'" :href="`mailto:${method.address}`" target="_blank">
+                <img src="@/assets/mail.svg" alt="mail icon" />
+              </a>
+              <a v-if="method.name == 'github'" :href="method.address" target="_blank">
+                <img src="@/assets/github.svg" alt="github icon" />
+              </a>
+            </div>
+          </div>
+        </article>
+      </div>
+    </main>
   </Layout>
 </template>
 
 <script>
 export default {
   metaInfo: {
-    title: 'Hello, world!'
+    title: "Inicio"
+  },
+  data() {
+    return {
+      users: [
+        {
+          name: "Bruno De Angelis",
+          role: "Multimedia Designer",
+          pic: "bruno.png",
+          contactMethods: [
+            { name: "mail", address: "brunordeangelis@gmail.com" },
+            { name: "github", address: "https://github.com/brunodeangelis" }
+          ]
+        },
+        {
+          name: "Franco Tarelli",
+          role: "Multimedia Designer",
+          pic: "franco.png",
+          contactMethods: [{ name: "mail", address: "tarelli39@gmail.com" }]
+        }
+      ]
+    };
   }
-}
+};
 </script>
 
 <style>
-.home-links a {
-  margin-right: 1rem;
+figure img {
+  transition: filter 200ms ease;
+  filter: grayscale(1);
+}
+
+article:hover figure img {
+  filter: grayscale(0);
 }
 </style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,14 @@
 module.exports = {
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        primary: "#006FAE"
+      }
+    },
+    fontFamily: {
+      sans: ["Poppins", "sans-serif"]
+    }
   },
   variants: {},
   plugins: []
-}
+};
