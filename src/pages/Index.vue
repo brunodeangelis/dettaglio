ef69e6e256461cd1faebe5c89d79c6bebbbaae8a
Bruno De Angelis <41843151+brunodeangelis@users.noreply.github.com>
2019-08-02 15:27:43 -0300

    added nicbritos. fixed layout.

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -5,25 +5,29 @@
         <g-image src="@/assets/logo.png" alt="logo Dettaglio" />
       </div>
       <div class="text-center mt-8">
-        <h1 class="text-4xl text-primary">We create digital products</h1>
+        <h1 class="text-4xl text-primary">Digital agency based in Argentina</h1>
       </div>
     </header>
     <main class="mt-16">
-      <div class="flex flex-col md:flex-row md:justify-center">
+      <div class="flex flex-col flex-wrap md:flex-row md:justify-center">
         <article
           v-for="(user, i) in users"
           :key="i"
-          class="mb-12 mx-auto md:mb-0 md:px-6 lg:px-8 xl:px-16"
+          class="mb-12 mx-auto sm:w-8/12 md:w-1/2 lg:w-1/3 lg:mb-0 md:px-6 lg:px-8"
         >
           <figure>
-            <g-image
-              :src="`/img/${user.pic}`"
-              :alt="`profile photo of ${user.name}`"
-              class="rounded-lg mb-4"
-            />
+            <div class="mb-4">
+              <g-image
+                :src="`/img/${user.pic}`"
+                :alt="`profile photo of ${user.name}`"
+                class="rounded-lg"
+              />
+            </div>
             <figcaption>
-              <h2 class="text-4xl text-primary font-bold">{{ user.name }}</h2>
-              <h3 class="tracking-wide uppercase text-xl text-gray-600">{{ user.role }}</h3>
+              <h2 class="text-4xl text-primary font-bold leading-tight">{{ user.name }}</h2>
+              <h3
+                class="leading-tight tracking-wide uppercase text-xl text-gray-600"
+              >{{ user.role }}</h3>
             </figcaption>
           </figure>
           <div class="flex mt-4">
@@ -64,6 +68,15 @@ export default {
           role: "Multimedia Designer",
           pic: "franco.png",
           contactMethods: [{ name: "mail", address: "tarelli39@gmail.com" }]
+        },
+        {
+          name: "Nicolás Britos",
+          role: "Computer Engineer",
+          pic: "nico.jpeg",
+          contactMethods: [
+            { name: "mail", address: "nicbritos@hotmail.com" },
+            { name: "github", address: "https://github.com/nicbritos" }
+          ]
         }
       ]
     };
@@ -75,9 +88,22 @@ export default {
 figure img {
   transition: filter 200ms ease;
   filter: grayscale(1);
+  width: 100%;
 }
 
 article:hover figure img {
   filter: grayscale(0);
+}
+
+@media (min-width: 430px) {
+  figure img {
+    width: 48rem;
+  }
+}
+
+@media (max-width: 992px) {
+  figure img {
+    filter: grayscale(0);
+  }
 }
 </style>
